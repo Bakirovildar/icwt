6b613a617cfb33931f2348836d68a576bc6ba015
Bakirovildar <bakirovildar2011@mail.ru>
2023-02-19 10:24:43 +0500

    добавил иконку, для компонента карточки

--- src/components/CardComponent/CardMain/CardMain.tsx
+++ src/components/CardComponent/CardMain/CardMain.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
+import {FavoriteIcon} from "../../../assests/icons/Favorite";
 
 const MainWrapper = styled.div`
   width: 100%;
   height: 260px;
+  position: relative;
+`
+
+const IconFavorite = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `
 
 function CardMain() {
     return (
         <MainWrapper>
-
+            <IconFavorite>
+                <FavoriteIcon/>
+            </IconFavorite>
         </MainWrapper>
     );
 }
