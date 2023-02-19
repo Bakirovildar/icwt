32e8b57e0e9acab1379ca02efe95aaa601de8059
Bakirovildar <bakirovildar2011@mail.ru>
2023-02-19 14:09:11 +0500

    реализация вывода шуток через каждые три секунды и остановка

--- src/components/CardComponent/CardFooter/CardFooter.tsx
+++ src/components/CardComponent/CardFooter/CardFooter.tsx
@@ -1,8 +1,20 @@
-import React, {useReducer} from 'react';
+import React, {Dispatch, useEffect, useReducer} from 'react';
 import {Button, FooterWrapper} from '../CardMain/StyledMain';
+import {useDispatch} from "react-redux";
+import {getJoke} from "../../../store/action";
 
 function CardFooter() {
     const [isStopJoke, setIsStopJoke] = useReducer(isStopJoke => !isStopJoke, false)
+    const dispatch: Dispatch<any> = useDispatch()
+
+    useEffect(() => {
+        if (!isStopJoke) return
+        let interval = setInterval(() => {
+            dispatch(getJoke())
+        }, 3000)
+
+        return () => clearInterval(interval)
+    }, [isStopJoke])
 
     return (
         <FooterWrapper>
