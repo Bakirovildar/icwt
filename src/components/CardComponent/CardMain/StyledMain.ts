2ac66867d32981645813732ab386ad6852523405
Bakirovildar <bakirovildar2011@mail.ru>
2023-02-19 10:33:40 +0500

    перенес styled стиили в отдельный файл, для mainComponent

---
+++ src/components/CardComponent/CardMain/StyledMain.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  height: 40px;
+`
+
+export const Button = styled.button`
+  width: 120px;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid #c4c6cb;
+  background: #F6F8FA;
+  color: #24292F;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    background: #dadfe2;
+  }
+
+  &:active {
+    background: #cdd4d9;
+  }
+`
