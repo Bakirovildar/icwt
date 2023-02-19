097c58c32ca7152abf50e688e7946fcb97882a77
Bakirovildar <bakirovildar2011@mail.ru>
2023-02-19 10:03:59 +0500

    добавил компонент main для карточки

---
+++ src/components/CardComponent/CardMain/CardMain.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  width: 100%;
+  height: 260px;
+`
+
+function CardMain() {
+    return (
+        <MainWrapper>
+
+        </MainWrapper>
+    );
+}
+
+export default CardMain;
