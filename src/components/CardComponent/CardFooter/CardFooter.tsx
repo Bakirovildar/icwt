a88cca80b487b558a14ae54936a509b86635b669
Bakirovildar <bakirovildar2011@mail.ru>
2023-02-19 10:34:54 +0500

    лобавил логику для кнопки (показ, остановка)

--- src/components/CardComponent/CardFooter/CardFooter.tsx
+++ src/components/CardComponent/CardFooter/CardFooter.tsx
@@ -1,34 +1,14 @@
-import React from 'react';
-import styled from "styled-components";
-
-const FooterWrapper = styled.div`
-  width: 100%;
-  height: 40px;
-`
-
-const Button = styled.button`
-  width: 120px;
-  height: 30px;
-  border-radius: 5px;
-  border: 1px solid #c4c6cb;
-  background: #F6F8FA;
-  color: #24292F;
-  outline: none;
-  cursor: pointer;
-
-  &:hover {
-    background: #dadfe2;
-  }
-
-  &:active {
-    background: #cdd4d9;
-  }
-`
+import React, {useReducer} from 'react';
+import {Button, FooterWrapper} from '../CardMain/StyledMain';
 
 function CardFooter() {
+    const [isStopJoke, setIsStopJoke] = useReducer(isStopJoke => !isStopJoke, false)
+
     return (
         <FooterWrapper>
-            <Button>Показать шутку</Button>
+            <Button
+                onClick={() => setIsStopJoke()}
+            >{!isStopJoke ? 'Показать шутку' : 'Остановить'}</Button>
         </FooterWrapper>
     );
 }
